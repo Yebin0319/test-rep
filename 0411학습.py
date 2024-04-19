6ffed3cc2af73ce4f3fb4ecc216a4c9c59bf1ca0
Yebin0319 <163494030+Yebin0319@users.noreply.github.com>
2024-04-19 18:15:11 +0900

    Add files via upload

---
+++ 0411학습.py
@@ -0,0 +1,38 @@
+for i in range(1,4):
+    for j in range(1,6):
+        print(j, end='  ')
+    print()
+
+print()
+
+for i in range(1,4):
+    for j in range(1,6):
+        print(i*j, end='  ')
+    print()
+
+print()
+
+for i in range(1,4):
+    for j in range(1,6):
+        print(j+i, end='  ')
+    print()
+
+print()
+
+for i in range(3):
+    for j in range(1,6):
+        print(i*j, end='  ')
+    print()
+print()
+
+for i in range(1,10):
+    for j in range(2,5):
+        print(j,'*',i,'=',i*j, end=' ')
+    print()
+
+print()
+
+for i in range(1,10):
+    for j in range(2,5):
+        print('{:3}*{:3}={:3}'.format(j, i, i*j), end=' ')
+    print()
